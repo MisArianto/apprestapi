cdfb60621aeb0918209b35d71ffa648d7f5633c0
= <=>
2020-12-13 14:37:15 +0700

    login users

--- middleware/auth.js
+++ middleware/auth.js
@@ -43,3 +43,54 @@ exports.registrasi = (req, res) => {
         }
     })
 }
+
+// controller untuk login
+exports.login = (req, res) => {
+    let post = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    let query = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ?"
+    let table = ['users', 'password', bcrypt.compareSync(post.password, salt), 'email', post.email]
+
+    query = mysql.format(query, table)
+    connection.query(query, (error, rows) => {
+        if (error) {
+            console.log(error)
+        } else {
+            if (rows.length == 1) {
+                let token = jwt.sign({ rows }, config.secret, {
+                    expiresIn: 1440
+                })
+
+                user_id = rows[0].id
+
+                let data = {
+                    user_id: user_id,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                let query = "INSERT INTO ?? SET ?"
+                let table = ['akses_token']
+
+                query = mysql.format(query, table)
+                connection.query(query, data, (error, rows) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        res.json({
+                            success: true,
+                            message: 'Token JWT tergenerate!',
+                            token: token,
+                            currentUser: data.user_id
+                        })
+                    }
+                })
+            } else {
+                res.json({ 'Error': true, 'Message': 'Email atau Password Salah!' })
+            }
+        }
+    })
+}
